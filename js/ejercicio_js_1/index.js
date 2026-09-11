325f41a2ea01145f035b5419e7e31c03a3996131
Emanuel Razo <manuelrazo8909@gmail.com>
2026-09-10 22:21:21 -0600

    commit 10/09/26

--- js/ejercicio_js_1/index.js
+++ js/ejercicio_js_1/index.js
@@ -49,11 +49,11 @@ var c = 4;
 var resultadoOperacion = ((a+b) * (c))/c;
 console.log (((a+b) * (c)) / c);
 console.log (resultadoOperacion);
-
+//asignar valor a variables previamente declaradas//
 var x;
 var y;
 
-x = 5+6;
+x = 5+6; //guarda el numero 11
 y = x * 10;
 
 console.log ("otra prueba");
@@ -89,5 +89,82 @@ console.log("variable4:" + variable4);
 
 console.log("2. CONST ****");
 const VALOR_GRAVEDAD_TIERRA = 9.81;
-//VALOR_GRAVEDAD_TIERRA = 6;//
-console.log("VALOR_GRAVEDAD_TIERRA=" + VALOR_GRAVEDAD_TIERRA)
+//VALOR_GRAVEDAD_TIERRA = 6;// En los constantes no se puede volver a reasignar un valor
+console.log("VALOR_GRAVEDAD_TIERRA=" + VALOR_GRAVEDAD_TIERRA);
+
+
+
+console.log("**********************");
+console.log("**********************");
+console.log("**********************");
+console.log("**********************");
+
+
+
+console.log("***METODOS O FUNCIONES***")
+
+var n1 = 6;
+var n2 = 7;
+var n3 = 56;
+
+
+//SIN FUNCIONES
+console.log("operciones sin usar FUNCIONES");
+
+var rasultado_calculo1 = ((n1+n2)*n3) -2;
+console.log("mi_resultado_calculo_1 = ", rasultado_calculo1);
+
+var rasultado_calculo2 = ((n1+n2)*n3) -4;
+console.log("mi_resultado_calculo_2 = ", rasultado_calculo2);
+
+var rasultado_calculo3 = ((n1+n2)*n3) -6;
+console.log("mi_resultado_calculo_3 = ", rasultado_calculo3);
+
+var rasultado_calculo4 = ((n1+n2)*n3) -3;
+console.log("mi_resultado_calculo_4 = ", rasultado_calculo4);
+
+var rasultado_calculo5 = ((n1+n2)*n3) -1;
+console.log("mi_resultado_calculo_5 = ", rasultado_calculo5);
+
+//EJEMPLO DE UNA FUNCION
+/*
+ function apagarFoco() {
+    //el circuito electrico desactiva la corriente y apaga el foco
+ }
+function impromirNombreEnDocumentoA(nombreAlumno){
+    //La encargada de servicio escolares abre el pdf, o word de plantilla.
+    //escribe el nombre recibido, manda imprimir, espera a que termine de imprimir
+
+    return la encargada regrasa el documento impreso con el nombre que ella recibio;
+
+}
+*/
+
+function FuncionDeCalcular (num1, num2, num3){
+    
+    //linea 1
+    //linea 2
+    //forma A
+
+    /*let operacionResultaEjemplo = ((numero1+munero2)*numero3)-2;
+    return operacionResultaEjemplo:
+    */
+
+    //forma B , regresar solamente el resultado
+    retur ((numero1+numero2)* numero3)-2;
+}
+
+var resultado_calculo1 = FuncionDeCalcular(n1,n2,n3);
+console.log("al_resultado_calculo1= ", resultado_calculo1);
+
+var resultado_calculo2 = FuncionDeCalcular(8,6,9);
+console.log("al_resultado_calculo2= ", resultado_calculo2);
+
+var resultado_calculo3 = FuncionDeCalcular(2,1,1);
+console.log("al_resultado_calculo3= ", resultado_calculo3);
+
+var resultado_calculo4 = FuncionDeCalcular(45,65,77);
+console.log("al_resultado_calculo4= ", resultado_calculo4);
+
+var resultado_calculo5 = FuncionDeCalcular(11,22,23);
+console.log("al_resultado_calculo5= ", resultado_calculo5);
